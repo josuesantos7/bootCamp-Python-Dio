7d4254efda71fa2016e011e7ea8d56f5be75363e
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2025-04-22 16:12:53 -0300

    Add Operadores Aritiméricos

---
+++ Tipos de Operadores/Operadores_Aritmeticos.py
@@ -0,0 +1,20 @@
+# Adição
+print(10 + 5)  # 15
+
+#subtração
+print(10 - 5)  # 5
+
+# Multiplicação
+print(10 * 5)  # 50
+
+# Divisão
+print(10 / 5)  # 2.0
+
+# Divisão inteira
+print(10 // 5)  # 2
+
+# Modulo (resto da divisão)
+print(10 % 5)  # 0
+
+# Potenciação
+print(10 ** 5)  # 100000
