5cba16557e0725da8f3ea8d8c6e84d411f843915
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2025-04-22 16:21:11 -0300

    add operadores de comparação.

---
+++ Operadores_de_Comparacao.py
@@ -0,0 +1,20 @@
+
+# Igualdade
+Saldo = 450
+saque = 200
+
+print(Saldo == saque) # False
+
+
+# Diferença
+print(Saldo != saque) # True
+
+
+# Maior que / Maior ou igual
+print(Saldo > saque) # True
+print(Saldo >= saque) # True
+
+
+# Menor que / Menor ou igual
+print(Saldo < saque) # False
+print(Saldo <= saque) # False
