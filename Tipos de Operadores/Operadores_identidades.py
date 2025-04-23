c52db8c734f7eab64b569e5ac63b4aabeb50fc41
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2025-04-23 17:11:06 -0300

    add operadores de identidade.

---
+++ Tipos de Operadores/Operadores_identidades.py
@@ -0,0 +1,13 @@
+# Operadores de ientidade:
+
+curso = "Curso de Python"
+nome_curso = curso
+saldo, limite = 200, 200
+
+# lê-se "Curso está em nome_curso".
+print (curso is nome_curso) # True
+
+print (curso is not nome_curso) # False
+
+print (saldo is limite) # True
+
