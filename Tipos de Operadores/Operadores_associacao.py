5c4d8bb9ef8321c2b52fe2c1db30636170ae29ca
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2025-04-23 18:00:13 -0300

    add Operadores de associação.

---
+++ Tipos de Operadores/Operadores_associacao.py
@@ -0,0 +1,12 @@
+# Operadores de Associação: in e not in
+curso = "Curso de Python"
+frutas = ["maçã", "banana", "laranja"]
+saques = [100, 1500]
+
+"Python" in curso # True
+
+"maçã" not in frutas # False
+
+"abacate" not in frutas # True
+
+200 in saques # False
