bd82c309716e0fcec240f6843ada9e8a56ca3e36
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2025-04-23 16:09:23 -0300

    add operadores de atribuição.

---
+++ Tipos de Operadores/Operadores_Atribuicao.py
@@ -0,0 +1,27 @@
+# Atribuição Simples
+Saldo = 500
+print(Saldo)
+
+
+# Atribuição com Adição
+Saldo = 500
+Saldo += 200
+
+
+# Atribuição com divisão
+Saldo1 = 550
+Saldo1 /= 2
+print(Saldo1)
+
+
+# Atribuição com Modulo
+Saldo2 = 550
+Saldo2 %= 280
+print(Saldo2)
+
+
+# Atribuição com exponenciação
+Saldo3 = 80
+Saldo3 **= 2
+print(Saldo3)
+
