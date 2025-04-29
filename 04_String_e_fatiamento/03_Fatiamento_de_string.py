4da74114be1ab03b635d6b7a4add3a1157c73d97
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2025-04-28 23:23:10 -0300

    add fatiamento de string.

---
+++ 04_String_e_fatiamento/03_Fatiamento_de_string.py
@@ -0,0 +1,11 @@
+# Fatiamento de String:
+
+nome = "Fulano de Tal da Silva"
+
+nome[0]  # F
+nome[:9]  # Fulano de T
+nome[10:]  # Tal da Silva
+nome[10:20]  # Tal da Silva
+nome[0:20:2]  # Fln e a i
+nome[:]  # Fulano de Tal da Silva
+nome[::-1]  # avliS ad alT onaluF
