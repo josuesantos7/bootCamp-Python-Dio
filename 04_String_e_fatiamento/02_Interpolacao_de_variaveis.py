f4a53bf1a9c320e111326bb1dfc8634f8883a523
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2025-04-28 23:15:46 -0300

    Add métodos de Interpolação.

---
+++ 04_String_e_fatiamento/02_Interpolacao_de_variaveis.py
@@ -0,0 +1,18 @@
+# Interpolação de variáveis:
+
+# Método format:
+print("Utilizando método format:")
+
+nome = "Jhenifer"
+idade = 25
+profissao = "programadora"
+linguagem = "Python"
+
+print("Olá, meu nome é {}. Tenho {} anos e sou {} em {}.".format(nome, idade, profissao, linguagem))
+
+
+# Método f-string:
+print("Utilizando método f-string:")
+
+print(f"Olá, meu nome é {nome}. Tenho {idade} anos e sou {profissao} em {linguagem}.")
+
