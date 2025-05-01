8609218d5f982a194333f95517c9cb7e17a94155
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2025-05-01 18:31:28 -0300

    Add o uso de string em multiplas linhas.

---
+++ 04_String_e_fatiamento/04_String_multiplas_linhas.py
@@ -0,0 +1,22 @@
+# String de multipas linhas:
+# uso das 3 aspas simples.
+
+nome = "Josué Santos"
+
+mensagem = f"""
+Olá, meu nome é {nome},
+Eu estou relembrando os conceitos de Python.
+"""
+
+print(mensagem)
+
+print("""
+    ############# Menu #############
+      1 - Depositar
+      2 - Sacar
+      0 - Sair
+
+    ################################
+      
+      Obrigado por usar nosso sistema!
+""")
