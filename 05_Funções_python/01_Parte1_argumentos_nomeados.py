942e7ace9f0e1003547e7c14d08ebf7d580bfbf9
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2025-05-06 16:30:56 -0300

    add argumentos nomeados.

---
+++ 05_Funções_python/01_Parte1_argumentos_nomeados.py
@@ -0,0 +1,6 @@
+# Argumentos nomeados:
+
+def salvar_carro(marca, modelo, ano, placa):
+    print(f'Carro: {marca} / {modelo} / {placa} / {ano}')
+
+salvar_carro(marca='Fusca', modelo='Fusca', ano=1970, placa='ABC-1234')
