e32316a8ce1455fc380ae80f918bef4f8ebd26b1
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2025-05-09 15:21:59 -0300

    Add parametros kwargs e da forma hibrida

--- 05_Funções_python/02_Parte2.py
+++ 05_Funções_python/02_Parte2.py
@@ -7,3 +7,18 @@ def criar_carro(modelo, ano, placa, /, marca, motor, combustivel):
     print(modelo, ano, placa, marca, motor, combustivel)
 
 criar_carro('Fusca', 1970, 'ABC-1234', marca='Volkswagen', motor='1.0', combustivel='Gasolina')
+
+print('---' * 10)
+# Parametros nomeados (Keyword-only arguments)
+def criar_carro2(*, modelo, ano, placa, marca, motor, combustivel):
+    print(modelo, ano, placa, marca, motor, combustivel)
+
+criar_carro2(modelo='Jetta', ano=2025, placa='ABC-1234', marca='Volkswagen', motor='2.0', combustivel='Gasolina')
+
+
+print('---' * 10)
+# Parametros variaveis hibridos(args e kwargs)
+def criar_carro3(modelo, ano, placa, /, *, marca, motor, combustivel):
+    print(modelo, ano, placa, marca, motor, combustivel)
+
+criar_carro3('Jetta', 2025, 'ABC-1234', marca='Volkswagen', motor='2.0', combustivel='Gasolina')
